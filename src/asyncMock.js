45d469e62067756b84c6b7fe313e784a70a1b6d0
Tam S C <artamcrafts@hotmail.com>
2024-08-31 04:39:01 -0300

    Pre-Entrega2-TamaraCanzobre

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -1,6 +1,9 @@
 import agendaFriends from './assets/agAnFriends.jpg';
 import stickersFriends from './assets/stickersFriends.jpg';
 import boxFriends from './assets/boxFriends.jpg';
+import agendaBttF from './assets/agAnBttF.jpg';
+import stickersBttF from './assets/stickersBttF.jpg';
+import boxBttF from './assets/boxBttF.jpg';
 
 const products = [
     {
@@ -13,6 +16,14 @@ const products = [
     },
     {
       id: 2,
+      titulo: 'Agenda/Anotador BttF',
+      precio: '8000',
+      categoria: 'Agendas y Anotadores',
+      descripcion: 'Podrás elegir entre agenda perpetua o anotador A5 o A6',
+      imagen: agendaBttF,
+    },
+    {
+      id: 3,
       titulo: 'Stickers Modelo: Friends',
       precio: '900',
       categoria: 'Stickers',
@@ -20,7 +31,23 @@ const products = [
       imagen: stickersFriends,
     },
     {
-      id: 3,
+      id: 4,
+      titulo: 'Stickers Modelo: BttF',
+      precio: '900',
+      categoria: 'Stickers',
+      descripcion: 'Stickers troquelados a prueba de agua',
+      imagen: stickersBttF,
+    },
+    {
+      id: 5,
+      titulo: 'Stationary Box - BttF',
+      precio: '12000',
+      categoria: 'boxes',
+      descripcion: 'Caja de papelería con 10 ítems!',
+      imagen: boxBttF,
+    },
+    {
+      id: 6,
       titulo: 'Stationary Box - Friends',
       precio: '12000',
       categoria: 'boxes',
