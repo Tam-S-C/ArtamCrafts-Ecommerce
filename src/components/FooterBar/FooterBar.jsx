4589548039b9909a2a7d2458f463a8768c07f4f0
Tam S C <artamcrafts@hotmail.com>
2024-09-03 00:56:22 -0300

    footer cambios

--- src/components/FooterBar/FooterBar.jsx
+++ src/components/FooterBar/FooterBar.jsx
@@ -12,27 +12,27 @@ export default function FooterBar() {
         <p style={{fontWeight:"bold", fontFamily:"monospace", fontSize:14}}>By Tamara S. Canzobre - Realizado con React.js <img src={imgReact} width={16}/></p>
         <div className='iconosFooter'>
           <a href="https://www.instagram.com/artamcrafts/" target="_blank">
-            <img src="./src/assets/Icono_Instagram.png" width={24} alt="Link de Instagram"/>
+            <img src="../src/assets/Icono_Instagram.png" width={24} alt="Link de Instagram"/>
           </a>
 
           <a href="https://www.behance.net/tamaracanzobre" target="_blank">
-            <img src="./src/assets/Icono_Behance.png" width={24} alt="Link de Behance"/>
+            <img src="../src/assets/Icono_Behance.png" width={24} alt="Link de Behance"/>
           </a>
 
           <a href="https://www.youtube.com/@artamcrafts" target="_blank">
-            <img src="./src/assets/Icono_Youtube.png" width={24} alt="Link de Youtube"/>
+            <img src="../src/assets/Icono_Youtube.png" width={24} alt="Link de Youtube"/>
           </a>
 
           <a href="https://www.linkedin.com/in/tamara-canzobre/" target="_blank">
-            <img src="./src/assets/Icono_Linkedin.png" width={24} alt="Link de LinkedIn"/>
+            <img src="../src/assets/Icono_Linkedin.png" width={24} alt="Link de LinkedIn"/>
           </a>
 
           <a href="https://github.com/Tam-S-C" target="_blank">
-            <img src="./src/assets/Icono_GitHub.png" width={24} alt="Link a GitHub"/>
+            <img src="../src/assets/Icono_GitHub.png" width={24} alt="Link a GitHub"/>
           </a>
 
           <a href="mailto:artamcrafts@hotmail.com" target="_blank">
-            <img src="./src/assets/Icono_mail.png" width={24} alt="Link del mail"/>
+            <img src="../src/assets/Icono_mail.png" width={24} alt="Link del mail"/>
           </a>
 
         </div>
