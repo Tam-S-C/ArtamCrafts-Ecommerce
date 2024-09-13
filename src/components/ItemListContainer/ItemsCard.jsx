eccae221e5010f8b829ae43857a0bbfebba5aba7
Tam S C <artamcrafts@hotmail.com>
2024-09-12 23:48:42 -0300

    boton Like y conexion DB

--- src/components/ItemListContainer/ItemsCard.jsx
+++ src/components/ItemListContainer/ItemsCard.jsx
@@ -1,7 +1,27 @@
 import './ItemsCard.css';
 import { Link } from 'react-router-dom';
+import likeOFF from '../../assets/likeOFF.png';
+import likeON from '../../assets/likeON.png';
+import { useState, useEffect } from 'react';
 
 export default function ItemsCard({ titulo, precio, imagen, id, category }) {
+
+
+  const localStorageKey = `like-${id}`;
+
+  const [isLiked, setIsLiked] = useState(() => {
+    const savedLike = localStorage.getItem(localStorageKey);
+    return savedLike === 'true';
+  });
+
+  const likeBtn = () => {
+    const newLikeState = !isLiked;
+    setIsLiked(newLikeState);
+   
+    localStorage.setItem(localStorageKey, newLikeState);
+  };
+
+
   return (
     <>
         <div className='contCards'>
@@ -9,9 +29,23 @@ export default function ItemsCard({ titulo, precio, imagen, id, category }) {
             <h5>{titulo}</h5>
             <img src={imagen} alt={titulo} width={200}/>
             <br/>
-            <h6>Precio: ${precio}</h6>
+
+            <section className="likeYprecio">
+              <h6>Precio: ${precio}</h6> 
+
+              <button onClick={likeBtn}>
+                <img
+                  src={isLiked ? likeON : likeOFF} 
+                  alt="like"
+                  width={24}
+                  className='like'
+                />
+              </button>
+
+            </section>
+
             <button className='btnDetalles'>
-            <Link to={`/${category}/${id}`}>Ver detalles</Link>
+              <Link to={`/${category}/${id}`}>Ver detalles</Link>
             </button>
           </article>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,19 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyBLSqXmtoTXeoVWjqqnOXWBXvOyu1vWEZA",
+  authDomain: "artamcrafts-bd.firebaseapp.com",
+  projectId: "artamcrafts-bd",
+  storageBucket: "artamcrafts-bd.appspot.com",
+  messagingSenderId: "1098356967022",
+  appId: "1:1098356967022:web:41971ca635843e40e3887f"
+};
+
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
